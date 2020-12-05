ba830c7976c06d355cca31103c0424bd1036b21d
Heseral <artemka-sapelkin@mail.ru>
2020-12-05 19:49:11 +0300

    одна из педалек, пока без ног

--- src/model/bicycle/PedalAndHuman.java
+++ src/model/bicycle/PedalAndHuman.java
@@ -1,5 +1,7 @@
 package model.bicycle;
 
+import util.GlobalVar;
+
 import java.awt.*;
 
 public class PedalAndHuman {
@@ -12,7 +14,36 @@ public class PedalAndHuman {
     }
 
     public void process(Graphics graphics) {
+        updateState();
 
+        graphics.setColor(Color.BLACK);
+        double stateInRadians = getState() * GlobalVar.DOUBLE_PI;
+        int firstPedalXPos = getBicycle().getWheels().getSECOND_CENTER_X()
+                        - getBicycle().getWheels().getRADIUS()
+                        - 33
+                        + (int) Math.round(Math.cos(-stateInRadians) * 20);
+        int firstPedalYPos = getBicycle().getWheels().getSECOND_CENTER_Y()
+                + (int) Math.round(Math.sin(-stateInRadians) * 20);
+        int secondPedalXPos = 0;
+        int secondPedalYPos = 0;
+        graphics.fillRect(
+                firstPedalXPos,
+                firstPedalYPos,
+                20,
+                6
+        );
+        graphics.drawLine(
+                getBicycle().getWheels().getSECOND_CENTER_X() - getBicycle().getWheels().getRADIUS() - 25,
+                getBicycle().getWheels().getSECOND_CENTER_Y(),
+                firstPedalXPos + 10,
+                firstPedalYPos + 3
+                );
+    }
+
+    public void updateState() {
+        setPrevState(getState());
+        double temp = getState() + getBicycle().getSpeed() / 1000;
+        setState(temp - (int) temp);
     }
 
     public double getState() {
